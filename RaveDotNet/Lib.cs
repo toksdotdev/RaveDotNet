17bd2735a4d60d43dc02ceae044ec63dfccb05f7
Tochukwu Nkemdilim <nkemdilimtochukwu@gmail.com>
2018-02-28 01:21:00 +0100

    feat: add library files

--- RaveDotNet/Lib.cs
+++ RaveDotNet/Lib.cs
@@ -1,8 +1,85 @@
-﻿using System;
+﻿using RaveDotNet.Services;
+using System;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using RaveDotNet.CardCharge;
+using RaveDotNet.CardValidation;
+using RaveDotNet.Miscellaneous.Card;
+using RaveDotNet.Services.Encryption;
+using RaveDotNet.Services.JSONConverter;
 
 namespace RaveDotNet
 {
     public class Rave
     {
+        public static string LIVE_URL = "https://api.ravepay.co";
+        public static string DEMO_URL = "http://flw-pms-dev.eu-west-1.elasticbeanstalk.com";
+
+        private readonly Request _client;
+
+        public enum Mode{
+            Test,
+            Live
+        }
+
+        public enum Algorithm {
+            Des,
+        }
+
+        private string PrivateKey {get; set;}
+
+        private Rave3DesEncryption encryption;
+
+        //private Mode OperationMode { get; set; }
+
+        public Rave(string privateKey, Mode operationMode)
+        {
+            PrivateKey = privateKey;
+
+            encryption = new Rave3DesEncryption();
+
+            var baseAddress = operationMode.Equals(Mode.Live) ? new Uri(LIVE_URL) : new Uri(DEMO_URL);
+
+            _client = new Request(privateKey, baseAddress);
+        }
+
+        /// <summary>
+        /// Charge a card
+        /// </summary>
+        /// <param name="card">Card details of customer</param>
+        /// <param name="alg">Algorithm to use for validation</param>
+        /// <returns>Details of the outcome of the card charge</returns>
+        public async Task<CardChargeResponse> ChargeCardAsync(Card card, Algorithm alg)
+        {
+            var body = new {
+                PBFPubKey = PrivateKey,
+                client = encryption.EncryptData(PrivateKey, card.ToJson()),
+                alg = alg.ToString()
+            };
+
+            var response = await _client.SendRequest(Request.RequestMethod.Post, "flwv3-pug/getpaidx/api/charge", body.ToJson());
+
+            return CardChargeResponse.FromJson(response);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="transactionReference">The unique reference/ flwRef, unique to the particular transaction being carried out. It is generated for every transaction. This can be retrieved from the card charge response</param>
+        /// <param name="otp">A one time Pin sent to the customer’s phone and inputed by the customer for authentication.</param>
+        /// <returns>Details of the outcome of the card charge</returns>
+        public async  Task<CardValidationResponse> ValidateCardAsync(string transactionReference, int otp)
+        {
+            var body = new
+            {
+                PBFPubKey = PrivateKey,
+                transaction_reference = transactionReference,
+                otp
+            };
+
+            var response = await _client.SendRequest(Request.RequestMethod.Post, "/flwv3-pug/getpaidx/api/validatecharge", body.ToJson());
+
+            return CardValidationResponse.FromJson(response);
+        }
     }
 }
---
+++ RaveDotNet/Services/Request.cs
@@ -0,0 +1,95 @@
+﻿using Newtonsoft.Json.Converters;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using RaveDotNet.Services.JSONConverter;
+
+namespace RaveDotNet.Services
+{
+    internal class Request
+    {
+        private readonly string _privateKey;
+
+        public enum RequestMethod
+        {
+            Get,
+            Post,
+            Put,
+            Delete
+        };
+
+        private readonly HttpClient _client;
+
+        public Request(string privateKey, Uri baseApiAddress)
+        {
+            _privateKey = privateKey;
+
+            _client = new HttpClient()
+            {
+                BaseAddress = baseApiAddress,
+            };
+
+            _client.DefaultRequestHeaders.Accept.Clear();
+            _client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        /// <summary>
+        /// Sends http request to Host endpoint
+        /// </summary>
+        /// <param name="method">Method of request to send</param>
+        /// <param name="endpoint">Subset rave endpoint to send request</param>
+        /// <param name="body">Object data to send along with the http request</param>
+        /// <returns>String of json data containing the response details</returns>
+        public async Task<string> SendRequest(RequestMethod method, string endpoint, string body = null)
+        {
+            var result = string.Empty;
+            
+            var formsParams = new StringContent(body);
+
+            try
+            {
+                #region Send Request
+
+                HttpResponseMessage response;
+
+                switch (method)
+                {
+                    case RequestMethod.Get:
+                        response = await _client.GetAsync(endpoint);
+                        break;
+
+                    case RequestMethod.Post:
+                        response = await _client.PostAsync(endpoint, formsParams);
+                        response.EnsureSuccessStatusCode();
+                        break;
+
+                    case RequestMethod.Put:
+                        response = await _client.PutAsync(endpoint, formsParams);
+                        response.EnsureSuccessStatusCode();
+                        break;
+
+                    case RequestMethod.Delete:
+                        response = await _client.DeleteAsync(endpoint);
+                        break;
+
+                    default:
+                        throw new InvalidOperationException($"Invalid Request Method: {method}");
+                }
+
+                if (response != null && response.IsSuccessStatusCode)
+                {
+                    result = await response.Content.ReadAsStringAsync();
+                }
+
+                return result;
+
+                #endregion Send Request
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
