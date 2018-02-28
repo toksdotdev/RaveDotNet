4e70c17da6f0ef3da40624acd68b7c76adb13a62
Tochukwu Nkemdilim <nkemdilimtochukwu@gmail.com>
2018-02-28 01:21:44 +0100

    feat: add json converter handlers

---
+++ RaveDotNet/Services/JSONConverter/Serialize.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace RaveDotNet.Services.JSONConverter
+{
+    public static class Serialize
+    {
+        public static string ToJson<T>(this T self)
+        {
+            return JsonConvert.SerializeObject(self, Converter.Settings);
+        }
+    }
+}
